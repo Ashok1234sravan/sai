84f3b15d98a80f3e6af79f40bf2d65b25435bbea
harishkumar209 <javapuharish209@gmail.com>
2017-10-12 09:12:35 +0530

    spring
    bootstrap

---
+++ SpringTest/src/main/java/com/MyController.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController
+{
+   @RequestMapping("/")
+   public ModelAndView showFristPage()
+   {
+   ModelAndView obj=new ModelAndView ("index");
+   return obj;
+}
+}
